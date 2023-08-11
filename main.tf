38edb5d0f06755a7e5476acf2010dfe896c39eb8
Mohammed Fazil KK <109734112+MFazilKK@users.noreply.github.com>
2023-08-11 17:33:36 +0530

    Add files via upload

---
+++ main.tf
@@ -0,0 +1,24 @@
+provider "aws" {
+  region = "us-east-1"  # Replace with your desired region
+}
+
+resource "aws_kms_key" "database_master_key" {
+  description = "KMS key for RDS master user password"
+}
+
+resource "aws_db_instance" "rds01" {
+  allocated_storage    = 20
+  storage_type        = "gp2"
+  engine              = "mysql"
+  engine_version      = "5.7"
+  instance_class      = "db.t2.micro"
+  username            = "admin"
+  password            = "your_password" 
+
+  # Reference the KMS Key ID using the variable name aws_kms_key
+  master_user_secret_kms_key_id = aws_kms_key.database_master_key.id
+}
+
+output "rds_endpoint" {
+  value = aws_db_instance.rds01.endpoint
+}
